b132b4b889293955a04f4e14607523bda3961948
Brienyll <Brienyll29@gmail.com>
2021-11-14 10:24:28 -0800

    added button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import { Fragment } from 'react';
 
 import mealsimage from '../../assets/jimmy-dean-Yn0l7uwBrpw-unsplash.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsimage} alt="table with food" />
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCardButton.module.css'
+
+const HeaderCartButton = () => {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}><CartIcon /></span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>9</span>
+        </button>
+    )
+}
+
+export default HeaderCartButton;
