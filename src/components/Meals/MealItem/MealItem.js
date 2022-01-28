421d8ef5f650da9f5836951d1e0d38ad8f6390fc
Brienyll <Brienyll29@gmail.com>
2022-01-27 17:30:40 -0800

    output cart items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -12,7 +12,7 @@ const MealItem = props => {
             id: props.id,
             name: props.name,
             amount: amount,
-            prioce: props.price
+            price: props.price
         })
     }
     return (
